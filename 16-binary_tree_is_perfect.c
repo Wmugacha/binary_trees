f05291e8665bdca8b1a01ff4f400e3a123ced752
Wmugacha <wilfredmugacha@gmail.com>
2023-03-03 18:01:27 +0000

    perfect tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,18 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_perfect - function that checks if a binary tree is perfect.
- * @tree: pointer to the root node of the tree to check.
- * Return: 0 if tree is null.
- */
-
-#include "binary_trees.h"
-
-/**
  * binary_tree_is_full - checks if a tree has complete nodes
- *
  * @tree: pointer to the root tree
- *
  * Return: 0 if not full and 1 if full
  */
 

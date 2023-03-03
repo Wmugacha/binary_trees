1e39e88fa47fe4faef4845cbd14e2d93e019ba08
Wmugacha <wilfredmugacha@gmail.com>
2023-03-03 12:48:51 +0000

    task 10 error corrected

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,18 +8,14 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-
-	size_t rdepth = binary_tree_depth(tree->right);
-	size_t ldepth = binary_tree_depth(tree->left);
-
-	if (ldepth > rdepth)
+	if (tree)
 	{
-		return (ldepth + 1);
+		size_t l_depth, r_depth;
+
+		l_depth = (tree->left ? 1 + binary_tree_depth(tree->left) : 0);
+		r_depth = (tree->right ? 1 + binary_tree_depth(tree->right) : 0);
+
+		return (l_depth > r_depth ? l_depth : r_depth);
 	}
-	else
-	{
-		return (rdepth + 1);
-	}
+	return (0);
 }

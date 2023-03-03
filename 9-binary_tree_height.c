6425321753ce83831ac2a9eaf7a934f5c3e33c7e
Wmugacha <wilfredmugacha@gmail.com>
2023-03-03 12:33:47 +0000

    task 9 error corrected

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,8 +8,31 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+	if (tree)
+	{
+		size_t right_height, left_height;
 
-	return (binary_tree_height(tree->left) + 1 + binary_tree_height(tree->right));
+		if (tree->right)
+		{
+			right_height = (1 + binary_tree_height(tree->right));
+		}
+		else
+		{
+			right_height = 0;
+		}
+
+		if (tree->left)
+		{
+			left_height = (1 + binary_tree_height(tree->left));
+		}
+		else
+		{
+			left_height = 0;
+		}
+		if (right_height > left_height)
+			return (right_height);
+		else
+			return (left_height);
+	}
+	return (0);
 }
